7aa8703b085cbe77476737ac4489cf2dc563ad11
Woze Parrot <wozeparrot@gmail.com>
2024-05-23 20:36:40 -0700

    feat: disable rocm by default

--- flake.nix
+++ flake.nix
@@ -22,63 +22,8 @@
         ++ [
           (
             python-final: python-prev: {
-              tinygrad = python-prev.buildPythonPackage {
-                pname = "tinygrad";
-                version = inputs.tinygrad.shortRev;
-                pyproject = true;
-                src = inputs.tinygrad;
-
-                postPatch = ''
-                  # patch correct path to opencl
-                  substituteInPlace tinygrad/runtime/autogen/opencl.py --replace-fail "ctypes.util.find_library('OpenCL')" "'${prev.ocl-icd}/lib/libOpenCL.so'"
-
-                  # patch correct path to hip
-                  substituteInPlace tinygrad/runtime/autogen/hip.py --replace-fail "os.getenv('ROCM_PATH', '/opt/rocm/')+'/lib/libamdhip64.so'" "'${final.rocmPackages.clr}/lib/libamdhip64.so'"
-
-                  # patch correct path to comgr
-                  substituteInPlace tinygrad/runtime/autogen/comgr.py --replace-fail "os.getenv('ROCM_PATH')+'/lib/libamd_comgr.so' if os.getenv('ROCM_PATH') else ctypes.util.find_library('amd_comgr')" "'${final.rocmPackages.rocm-comgr}/lib/libamd_comgr.so'"
-                  substituteInPlace tinygrad/runtime/driver/hip_comgr.py --replace-fail "/opt/rocm/include" "${final.rocmPackages.clr}/include"
-
-                  # patch correct path to hsa
-                  substituteInPlace tinygrad/runtime/autogen/hsa.py --replace-fail "os.getenv('ROCM_PATH')+'/lib/libhsa-runtime64.so' if os.getenv('ROCM_PATH') else ctypes.util.find_library('hsa-runtime64')" "'${final.rocmPackages.rocm-runtime}/lib/libhsa-runtime64.so'"
-                '';
-
-                nativeBuildInputs = with python-prev; [
-                  setuptools
-                  wheel
-                ];
-
-                propagatedBuildInputs = with python-prev; [
-                  numpy
-                  tqdm
-                ];
-
-                pythonImportsCheck = ["tinygrad"];
-
-                nativeCheckInputs = with python-final; [
-                  hypothesis
-                  librosa
-                  onnx
-                  pillow
-                  pytest-xdist
-                  pytestCheckHook
-                  safetensors
-                  sentencepiece
-                  tiktoken
-                  torch
-                  transformers
-                  pkgs.llvmPackages_latest.clang
-                ];
-
-                preCheck = ''
-                  export HOME=$(mktemp -d)
-                  export CLANG=1
-                '';
-
-                pytestFlagsArray = [
-                  "-n auto"
-                  "test/test_ops.py"
-                ];
+              tinygrad = python-final.pythonPackages.callPackage ./tinygrad.nix {
+                inherit inputs;
               };
             }
           )
---
+++ tinygrad.nix
@@ -0,0 +1,75 @@
+{
+  lib,
+  inputs,
+  buildPythonPackage,
+
+  ocl-icd,
+  rocmPackages,
+
+  setuptools,
+  wheel,
+  numpy,
+  tqdm,
+
+  hypothesis,
+  torch,
+  pytest-xdist,
+  pytestCheckHook,
+  llvmPackages_latest,
+
+  enableOpenCL ? true,
+  enableRocm ? false,
+}:
+buildPythonPackage {
+  pname = "tinygrad";
+  version = inputs.tinygrad.shortRev;
+  pyproject = true;
+  src = inputs.tinygrad;
+
+  postPatch =
+    (lib.optionalString enableOpenCL ''
+      # patch correct path to opencl
+      substituteInPlace tinygrad/runtime/autogen/opencl.py --replace-fail "ctypes.util.find_library('OpenCL')" "'${ocl-icd}/lib/libOpenCL.so'"
+    '')
+    + (lib.optionalString enableRocm ''
+      # patch correct path to hip
+      substituteInPlace tinygrad/runtime/autogen/hip.py --replace-fail "os.getenv('ROCM_PATH', '/opt/rocm/')+'/lib/libamdhip64.so'" "'${rocmPackages.clr}/lib/libamdhip64.so'"
+
+      # patch correct path to comgr
+      substituteInPlace tinygrad/runtime/autogen/comgr.py --replace-fail "os.getenv('ROCM_PATH')+'/lib/libamd_comgr.so' if os.getenv('ROCM_PATH') else ctypes.util.find_library('amd_comgr')" "'${rocmPackages.rocm-comgr}/lib/libamd_comgr.so'"
+      substituteInPlace tinygrad/runtime/driver/hip_comgr.py --replace-fail "/opt/rocm/include" "${rocmPackages.clr}/include"
+
+      # patch correct path to hsa
+      substituteInPlace tinygrad/runtime/autogen/hsa.py --replace-fail "os.getenv('ROCM_PATH')+'/lib/libhsa-runtime64.so' if os.getenv('ROCM_PATH') else ctypes.util.find_library('hsa-runtime64')" "'${rocmPackages.rocm-runtime}/lib/libhsa-runtime64.so'"
+    '');
+
+  nativeBuildInputs = [
+    setuptools
+    wheel
+  ];
+
+  propagatedBuildInputs = [
+    numpy
+    tqdm
+  ];
+
+  pythonImportsCheck = ["tinygrad"];
+
+  nativeCheckInputs = [
+    hypothesis
+    torch
+    pytest-xdist
+    pytestCheckHook
+    llvmPackages_latest.clang
+  ];
+
+  preCheck = ''
+    export HOME=$(mktemp -d)
+    export CLANG=1
+  '';
+
+  pytestFlagsArray = [
+    "-n auto"
+    "test/test_ops.py"
+  ];
+}
