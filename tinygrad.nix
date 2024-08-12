79f44e0edcceaca89293f3b7eb2e0a6d9a9c907a
Woze Parrot <wozeparrot@gmail.com>
2024-08-12 15:19:19 -0700

    fix: correct patching after upstream changes

--- tinygrad.nix
+++ tinygrad.nix
@@ -44,15 +44,15 @@ buildPythonPackage {
       substituteInPlace tinygrad/runtime/autogen/hip.py --replace-fail "os.getenv('ROCM_PATH', '/opt/rocm/')+'/lib/libamdhip64.so'" "'${rocmPackages.clr}/lib/libamdhip64.so'"
 
       # patch correct path to comgr
-      substituteInPlace tinygrad/runtime/autogen/comgr.py --replace-fail "os.getenv('ROCM_PATH')+'/lib/libamd_comgr.so' if os.getenv('ROCM_PATH') else ctypes.util.find_library('amd_comgr')" "'${rocmPackages.rocm-comgr}/lib/libamd_comgr.so'"
-      substituteInPlace tinygrad/runtime/driver/hip_comgr.py --replace-fail "/opt/rocm/include" "${rocmPackages.clr}/include"
+      substituteInPlace tinygrad/runtime/autogen/comgr.py --replace-fail "/opt/rocm/lib/libamd_comgr.so" "${rocmPackages.rocm-comgr}/lib/libamd_comgr.so"
+      substituteInPlace tinygrad/runtime/support/compiler_hip.py --replace-fail "/opt/rocm/include" "${rocmPackages.clr}/include"
 
       # patch correct path to hsa
       substituteInPlace tinygrad/runtime/autogen/hsa.py --replace-fail "os.getenv('ROCM_PATH')+'/lib/libhsa-runtime64.so' if os.getenv('ROCM_PATH') else ctypes.util.find_library('hsa-runtime64')" "'${rocmPackages.rocm-runtime}/lib/libhsa-runtime64.so'"
     '')
     + (lib.optionalString cudaSupport ''
       # patch correct path to cuda
-      substituteInPlace tinygrad/runtime/autogen/cuda.py --replace-fail "ctypes.util.find_library('nvrtc')" "'${cudaPackages.cuda_nvrtc.lib}/lib/libnvrtc.so'"
+      substituteInPlace tinygrad/runtime/autogen/nvrtc.py --replace-fail "ctypes.util.find_library('nvrtc')" "'${cudaPackages.cuda_nvrtc.lib}/lib/libnvrtc.so'"
       substituteInPlace tinygrad/runtime/autogen/cuda.py --replace-fail "ctypes.util.find_library('cuda')" "'${cudaPackages.cuda_cudart}/lib/libcuda.so'"
     '');
 
