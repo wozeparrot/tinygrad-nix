dee39444d80b78df7fd9d011abf8304de04d20f0
Woze Parrot <wozeparrot@gmail.com>
2025-12-04 19:41:59 -0800

    fix: patch for latest tinygrad

--- tinygrad.nix
+++ tinygrad.nix
@@ -34,18 +34,8 @@ buildPythonPackage {
       # copy extra into core tinygrad
       cp -r ${inputs.tinygrad}/extra ./tinygrad/
 
-      # recursively find all packages
-      paths=$(find tinygrad -type d -name '__pycache__' -prune -o -type d -name 'assets' -prune -o -type d -name 'installer' -prune -o -type d -print)
-      # replace / in path with .
-      paths=$(echo $paths | sed 's/\//\./g')
-      # write paths to file
-      for path in $paths; do
-        echo "$path" >> packages.txt
-      done
-      cat packages.txt
-
-      # patch packages in setup.py to read from the file
-      sed -i "/^\s*packages = \[/,/^\s*\],/c\      packages=open('./packages.txt').read().splitlines()," setup.py
+      sed -i '/^\s*packages = \[/,/^\s*\]/c\
+      packages = {find = {where = ["."], include = ["tinygrad*"], exclude = ["tinygrad.assets*", "tinygrad.installer*"]}}' pyproject.toml
 
       # patch all references to extra to tinygrad.extra
       files=$(find tinygrad -type f -name '__init__.py' -prune -o -type f -name '*.py' -print)
@@ -57,7 +47,7 @@ buildPythonPackage {
       substituteInPlace tinygrad/viz/serve.py --replace-fail "os.path.dirname(__file__)" '"${inputs.tinygrad}/tinygrad/viz/"'
 
       # patch libc
-      substituteInPlace tinygrad/runtime/autogen/libc.py --replace-fail "ctypes.util.find_library('c')" '"${stdenv.cc.libc}/lib/libc.so.6"'
+      substituteInPlace tinygrad/runtime/autogen/libc.py --replace-fail "find_library('c')" '"${stdenv.cc.libc}/lib/libc.so.6"'
 
       # patch gcc
       substituteInPlace tinygrad/runtime/support/system.py --replace-fail "ctypes.util.find_library('atomic')" '"${gcc.cc.lib}/lib/libatomic.so"'
@@ -68,23 +58,23 @@ buildPythonPackage {
     '')
     + (lib.optionalString openclSupport ''
       # patch correct path to opencl
-      substituteInPlace tinygrad/runtime/autogen/opencl.py --replace-fail "ctypes.util.find_library('OpenCL')" "'${ocl-icd}/lib/libOpenCL.so'"
+      substituteInPlace tinygrad/runtime/autogen/opencl.py --replace-fail "find_library('OpenCL')" "'${ocl-icd}/lib/libOpenCL.so'"
     '')
     + (lib.optionalString rocmSupport ''
       # patch correct path to hip
-      substituteInPlace tinygrad/runtime/autogen/hip.py --replace-fail "os.getenv('ROCM_PATH', '/opt/rocm/')+'/lib/libamdhip64.so'" "'${rocmPackages.clr}/lib/libamdhip64.so'"
+      substituteInPlace tinygrad/runtime/autogen/hip.py --replace-fail "os.getenv('ROCM_PATH', '/opt/rocm')" "'${rocmPackages.clr}'"
 
       # patch correct path to comgr
-      substituteInPlace tinygrad/runtime/autogen/comgr.py --replace-fail "/opt/rocm/lib/libamd_comgr.so" "${rocmPackages.rocm-comgr}/lib/libamd_comgr.so"
+      substituteInPlace tinygrad/runtime/autogen/comgr.py --replace-fail "os.getenv('ROCM_PATH', '/opt/rocm')" "'${rocmPackages.rocm-comgr}'"
       substituteInPlace tinygrad/runtime/support/compiler_amd.py --replace-fail "/opt/rocm/include" "${rocmPackages.clr}/include"
 
       # patch correct path to hsa
-      substituteInPlace tinygrad/runtime/autogen/hsa.py --replace-fail "os.getenv('ROCM_PATH')+'/lib/libhsa-runtime64.so' if os.getenv('ROCM_PATH') else ctypes.util.find_library('hsa-runtime64')" "'${rocmPackages.rocm-runtime}/lib/libhsa-runtime64.so'"
+      substituteInPlace tinygrad/runtime/autogen/hsa.py --replace-fail "os.getenv('ROCM_PATH', '/opt/rocm')" "'${rocmPackages.rocm-runtime}'"
     '')
     + (lib.optionalString cudaSupport ''
       # patch correct path to cuda
-      substituteInPlace tinygrad/runtime/autogen/nvrtc.py --replace-fail "ctypes.util.find_library('nvrtc')" "'${lib.getLib cudaPackages.cuda_nvrtc}/lib/libnvrtc.so'"
-      substituteInPlace tinygrad/runtime/autogen/cuda.py --replace-fail "ctypes.util.find_library('cuda')" "'${addDriverRunpath.driverLink}/lib/libcuda.so'"
+      substituteInPlace tinygrad/runtime/autogen/nvrtc.py --replace-fail "find_library('nvrtc')" "'${lib.getLib cudaPackages.cuda_nvrtc}/lib/libnvrtc.so'"
+      substituteInPlace tinygrad/runtime/autogen/cuda.py --replace-fail "find_library('cuda')" "'${addDriverRunpath.driverLink}/lib/libcuda.so'"
 
       # patch correct path to include
       substituteInPlace tinygrad/runtime/support/compiler_cuda.py \
